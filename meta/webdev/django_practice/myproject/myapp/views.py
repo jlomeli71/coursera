cf9fed955f661307c45da15fe1113f52d5bdb934
Jose M. Lomeli <jmlomeli@hotmail.com>
2023-10-30 16:29:31 -0600

    updating work day

--- meta/webdev/django_practice/myproject/myapp/views.py
+++ meta/webdev/django_practice/myproject/myapp/views.py
@@ -30,10 +30,14 @@ def drinks(request, drink_name):
     return HttpResponse(f"<h2>{drink_name}: " + choice_of_drink)
 
 def about(request):
-    return HttpResponse("About us")
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
+    return render(request, "about.html", {'content': about_content})
+    # return HttpResponse("About us")
 
 def menu(request):
-    return HttpResponse("Menu")
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
+    return render(request, "menu.html", {'content': about_content})
+    # return HttpResponse("Menu")
 
 def book(request):
     return HttpResponse("Make a booking")
--- meta/webdev/django_practice/myproject/myproject/settings.py
+++ meta/webdev/django_practice/myproject/myproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],          # added 'templates'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'feedback',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'admindjango',
+        'PASSWORD': 'password',
     }
 }
 
@@ -122,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    'myapp/static',
+]
