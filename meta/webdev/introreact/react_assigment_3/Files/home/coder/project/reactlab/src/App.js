bff83978eb4e629bedf1472624d1af7da1a755ab
Jose M. Lomeli <jmlomeli@hotmail.com>
2023-11-28 21:26:02 -0600

    react files course

---
+++ meta/webdev/introreact/react_assigment_3/Files/home/coder/project/reactlab/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName='Jose' />
+            <Heading firstName='Manuel' />
+        </div>
+    );
+};
+
+export default App;
